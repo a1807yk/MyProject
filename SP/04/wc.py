1a1e4c62de7c0648d9ed3e93d84ed48889b685cc
Yoshiaki Katsumata <a1807yk@aiit.ac.jp>
2018-06-23 10:27:24 +0900

    modify file open

---
+++ SP/04/wc.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def wc(f):
+    wordcount = {}
+    sum = 0
+    for line in f:
+        words = line.replace(',', '').replace('.', '').replace('\"', '').replace('!', '').replace('?', '').replace('(', '').replace(')', '').replace('--', '').replace('_', '').lower().split()
+        for word in words:
+            sum += 1
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] += 1
+    print('Number of words: ', sum)
+    print('Top 20 frequent words:')
+    arrays = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
+    i = 0
+    for array in arrays:
+        print(' ' + array[0], array[1])
+        i += 1
+        if i == 20:
+            break
+
+
+if len(sys.argv) > 1:
+    try:
+        f = open(sys.argv[1], mode='r', encoding='utf-8')
+    except IOError:
+        sys.exit('Error No such file or directory: \'%s\'' % sys.argv[1])
+else:
+    f = sys.stdin
+wc(f)
+f.close()
